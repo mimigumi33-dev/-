35ae84b8cb3e99252dde193bc59b290292d1d1b2
mimigumi33-dev <mimi.gumi33@gmail.com>
2026-09-25 02:14:05 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yandere-light-v5";
+const CACHE_NAME = "yandere-light-v6";
 const APP_FILES = ["./", "./index.html", "./style.css", "./script.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
